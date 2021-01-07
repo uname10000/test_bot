ec60237f2a3ecb508fdc682d914792c47fd701f4
sasha <alexanderostapenko24@gmail.com>
2021-01-07 21:52:13 +0200

    Add cart functionality

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from shop.api.resurses import CategoryResources
 import time
 from shop.bot import config
 
-bot.polling()
+# bot.polling()
 bot.remove_webhook()
-#time.sleep(0.5)
-#bot.set_webhook(config.WEBHOOK_URL, certificate=open('webhook_cert.pem'))
+time.sleep(0.5)
+bot.set_webhook(config.WEBHOOK_URL, certificate=open('webhook_cert.pem'))
 # app.run()
 
